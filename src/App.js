aeed1654a31cb103640e1d65c002ba3a058105e7
Amirkhan567 <amrkh5678@gmail.com>
2023-09-19 22:07:23 +0500

    faraway app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [txtValue, setTxtValue] = useState('');
+  const [items, setItems] = useState([]);
+  const [itemQty, setItemQty] = useState(1)
+ 
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Logo />
+    <Form value={setTxtValue} text={txtValue} items={setItems} itemVal={items} qtyVal={setItemQty} itemQty={itemQty} />
+    <PackingList selectedItems={items} />
+
+    </>
   );
+}
+
+function Logo(){
+  return(
+    <div className='header' style={{backgroundColor: "whitesmoke",color:"chocolate",textTransform: "uppercase",height: "80px", display: "flex", justifyContent:"center",alignItems:"center", border:"4px solid grey", fontSize:"24px", }}>
+      <h1>
+        far away
+      </h1>
+    </div>
+  )
+}
+
+
+function Form({value, text, items, itemQty, qtyVal, itemVal}){
+  return(
+    <div style={{display: "flex", justifyContent: "center", alignItems: "center", textTransform: "capitalize", minHeight:"60px", backgroundColor: "beige", gap:"5px", flexWrap: "wrap", padding: "10px 0px"}}
+    >
+      <p>what do you need for your <img src='' /> trip?</p>
+{/* dropdown */}
+    <select style={{width: "40px", padding: "1px"}} value={itemQty} onChange={(e)=> qtyVal(parseInt(e.target.value))}>
+    {Array.from(Array(20).keys()).map((x) => (
+            <option key={x} value={x + 1}>{x + 1}</option>
+          ))}
+  </select>
+{/* input */}
+      <input type='text' onChange={(e)=> value(e.target.value) } value={text} />
+
+{/* button */}
+      <button style={{textTransform: "uppercase", width: "60px", backgroundColor: "skyblue", border: "2px solid skyblue", borderRadius: "10px", color: "white", fontWeight: "600", cursor: "pointer"}} onClick={() => {
+        items([{name: text, qty: itemQty}, ...itemVal])
+        value("")
+        qtyVal(1)
+      }}  >add</button>
+
+    </div>  
+  )
+}
+
+function PackingList({selectedItems}){
+
+  return(
+<>
+{
+  selectedItems.map((value, indx)=>{
+   return  <div key={indx} style={{display:"flex", justifyContent:"center", gap:"10px", padding:"10px 0px"}}><p>{value.qty}</p> <p>{value.name}</p></div>
+  })
+}
+</>
+  )
 }
 
 export default App;
